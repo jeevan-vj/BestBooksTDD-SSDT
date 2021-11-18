465cd9312587046bba19520aad76b278ebceae89
jeevan wijerathna <jeevan90wijerathna@gmail.com>
2021-11-18 16:57:20 +0800

    Update BuildSolution.ps1

--- BuildSolution.ps1
+++ BuildSolution.ps1
@@ -2,7 +2,7 @@ $root = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
 $msBuildRootDir = "$root\MSBuild"
 Write-Host "Root Path: " $root
 
-$msbuildPath = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin\\msbuild.exe"
+# $msbuildPath = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin\\msbuild.exe"
 
 
 if(!(Test-Path ./nuget.exe)){
@@ -10,8 +10,8 @@ if(!(Test-Path ./nuget.exe)){
 }
 ./nuget install Microsoft.Data.Tools.Msbuild -o  $msBuildRootDir
 
-$msbuildPath2 = Get-Childitem –Path $msBuildRootDir  -Include 'msbuild.exe'  -File -Recurse | Select-Object -First 1
-Write-Host $msbuildPath2
+$msbuildPath = Get-Childitem –Path $msBuildRootDir  -Include 'msbuild.exe'  -File -Recurse | Select-Object -First 1
+Write-Host $msbuildPath
 
 Write-Host "Testing path: " $msbuildPath
 
@@ -23,6 +23,19 @@ if(!(Test-Path $msbuildPath)){
 Write-Host "Using path: " $msbuildPath
 
 & $msbuildPath .\MyBestBooks\\MyBestBooks.sln
+
+$SqlPackageLocation2 = "C:\jeevan\Softwares\sqlpackage-win7-x64-en-US-15.0.5084.2\sqlpackage.exe"
+$buildOutPutDacpacFilePath = (Join-Path $root "MyBestBooks\BestBooks\bin\Debug\BestBooks.dacpac")
+Write-Host $buildOutPutDacpacFilePath
+if(!(Test-Path $buildOutPutDacpacFilePath)){
+    Write-Host "Can't find dacpac"  
+    Exit(1)
+}
+
+& $SqlPackageLocation2 /action:Publish  /SourceFile:$buildOutPutDacpacFilePath /TargetDatabaseName:BestBooks /TargetServerName:'localhost' /tu:sa /tp:123 
+
+Write-Host "Successfully deployed"
+
 if(!($?) -or $lastExitCode -ne 0){
     exit(1)
 }
