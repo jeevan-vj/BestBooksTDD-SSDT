c315958326b781451d4714abf22f41596028f9c9
jeevan wijerathna <jeevan90wijerathna@gmail.com>
2021-11-19 11:46:22 +0800

    Update BuildSolution.ps1

--- BuildSolution.ps1
+++ BuildSolution.ps1
@@ -2,7 +2,7 @@ $root = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
 $msBuildRootDir = "$root\MSBuild"
 Write-Host "Root Path: " $root
 
-# $msbuildPath = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin\\msbuild.exe"
+ $msbuildPath = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\MSBuild\\Current\\Bin\\msbuild.exe"
 
 
 if(!(Test-Path ./nuget.exe)){
@@ -10,8 +10,8 @@ if(!(Test-Path ./nuget.exe)){
 }
 ./nuget install Microsoft.Data.Tools.Msbuild -o  $msBuildRootDir
 
-$msbuildPath = Get-Childitem –Path $msBuildRootDir  -Include 'msbuild.exe'  -File -Recurse | Select-Object -First 1
-Write-Host $msbuildPath
+#$msbuildPath = Get-Childitem –Path $msBuildRootDir  -Include 'msbuild.exe'  -File -Recurse | Select-Object -First 1
+#Write-Host $msbuildPath
 
 Write-Host "Testing path: " $msbuildPath
 
