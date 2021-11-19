43c7ea76657a382db2b3bf0cc50a553436715823
jeevan wijerathna <jeevan90wijerathna@gmail.com>
2021-11-19 11:50:10 +0800

    Update Book.sql

--- MyBestBooks/BestBooks/Tables/Book.sql
+++ MyBestBooks/BestBooks/Tables/Book.sql
@@ -2,7 +2,8 @@
 (
 	[BookId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	[BookTitle] VARCHAR(50),
-	[BookTypeId] INT NOT NULL 
+	[BookTypeId] INT NOT NULL,
+	[ColByJeevan] VARCHAR(50)
 	CONSTRAINT FK_BookType_Book FOREIGN KEY (BookTypeId) REFERENCES BookType(BookTypeId)
  ); 
  
